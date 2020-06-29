31f871b9f5b9d83766f2717b3f2132daa1516c60
Magu <hiiammagu@gmail.com>
2020-06-29 16:00:46 -0400

    Inicio de creacion de las vistas de contactos

---
+++ view/contacts/contact.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="../../../phoneGestor/css/compile/main.css">
+  <title>PhoneGestor</title>
+</head>
+<body>
+  <div class="div">
+    <header>
+      <a href="#" class="btn btn-back">inicio</a>
+      <h2 class="text">Mis Contactos</h2>
+    </header>
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Nombre</th>
+          <th>Telefóno</th>
+          <th>Editar</th>
+          <th>Eliminar</th>
+        </tr>
+      </thead>
+      <tbody>
+        
+      </tbody>
+    </table>
+  </div>
+  <?php include_once('c:/xampp/htdocs/phoneGestor/includes/footer.php'); ?>
+</body>
+</html>
