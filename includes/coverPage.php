3882b83a1f4c2cf3c5bc8e83c53fd74cc1bc03e2
Magu <hiiammagu@gmail.com>
2020-07-01 14:00:40 -0400

    Creando la zona principal

---
+++ includes/coverPage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet"  href="../../phoneGestor/css/compile/main.css">
+  <title>PhoneGestor</title>
+</head>
+<body>
+  <main class="slider">
+    <div id="sl-1" class="slide-item">
+      <h1>Phone Gestor</h1>
+      <h2>El Gestor de números de telefono número 1 del Mundo</h2>
+    </div>
+    <div id="sl-2" class="slide-item">
+      <h2>Máxima seguridad</h2>
+      <p>El gestor de números de telefono más seguro del múndo, con cifrado hecho por nuestros expertos en seguridad informatica capas de evitar cualquier tipo de robo de cuentas, o urto de tus número de telefono</p>
+    </div>
+    <div id="sl-3" class="slide-item">
+      <h2>Facil de utilizar</h2>
+      <p>Esta herramienta te permitira mantener tus números de telefonos seguros y disponibles en todo momento, pero tambien te permite gestionar los mismos de una manera rapida, eficaz y eficientemente, sin necesidad de realizar gran cantidad de cosas</p>
+    </div>
+  </main>
+</body>
+</html>
